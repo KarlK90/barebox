773d0f9e21b25260baf7df8b05ebbd3775f945d6
Stefan Kerkmann <karlk90@pm.me>
2024-10-28 20:51:34 +0100

    riscv: boards: sophgo: add milkv duo s

---
+++ arch/riscv/boards/sophgo/lowlevel.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <common.h>
+#include <asm/barebox-riscv.h>
+#include <debug_ll.h>
+
+ENTRY_FUNCTION(start_milkv_duo_s, a0, a1, a2)
+{
+	extern char __dtb_z_cv1812h_milkv_duo_s_start[];
+
+	putc_ll('>');
+
+	barebox_riscv_supervisor_entry(0x80000000, SZ_512M, a0, __dtb_z_cv1812h_milkv_duo_s_start + get_runtime_offset());
+}
