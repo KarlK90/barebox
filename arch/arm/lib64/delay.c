3f186bf5d0cfa6463dabe16d760b24451cbef156
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2025-01-13 17:36:52 +0100

    ARM64: lib64: pbl: implement get_time_ns and is_timeout
    All ARMv8-A cores implement the 64bit wide generic timer CNTPCT[1], thus
    we can use it to implement the get_time_ns and is_timeout helpers which
    in turn enable the whole read_poll_timeout class of functions in the
    PBL. As it is guaranteed that the timer will not wrap for 40 years no
    overflow handling is necessary.
    
    [1]: See "ARM Architecture Reference Manual for A-profile architecture
    (rev L.a)", chapter D12 "The Generic Timer in AArch64 state"
    
    Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

--- arch/arm/lib64/delay.c
+++ arch/arm/lib64/delay.c
@@ -3,6 +3,7 @@
 #include <asm/system.h>
 #include <clock.h>
 #include <common.h>
+#include <linux/math64.h>
 
 void udelay(unsigned long us)
 {
@@ -16,4 +17,21 @@ void udelay(unsigned long us)
 void mdelay(unsigned long ms)
 {
 	udelay(ms * 1000);
+}
+
+uint64_t get_time_ns(void)
+{
+	uint64_t cntfrq = get_cntfrq();
+	uint64_t cntpct = get_cntpct();
+
+	/* CNTFRQ not programmed: degrade to infinite poll */
+	if (!cntfrq)
+		return 0;
+
+	return mul_u64_u32_div(cntpct, SECOND, cntfrq);
+}
+
+int is_timeout(uint64_t start, uint64_t time_offset_ns)
+{
+	return (int64_t)(start + time_offset_ns - get_time_ns()) < 0;
 }
