6ce9177d3700bab031194c3fba40212b1ad8ace8
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2025-01-15 09:42:52 +0100

    ARM: lib32: pbl: implement get_time_ns and is_timeout
    ARMv7-A cores can implement the optional 64bit wide generic timer
    CNTPCT[1]. If it is present we can use it to implement the get_time_ns
    and is_timeout helpers which in turn enable the whole read_poll_timeout
    class of functions in the PBL. As it is guaranteed that the timer won't
    wrap for 40 years no overflow handling is necessary.
    
    [1]: See "ARM Architecture Reference Manual ARMv7-A and ARMv7-R edition
    (rev C.d)", Chapter B8 "The Generic Timer"
    
    Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

--- arch/arm/lib32/arm_architected_timer.c
+++ arch/arm/lib32/arm_architected_timer.c
@@ -10,8 +10,22 @@ void arm_architected_timer_udelay(unsigned long us)
 	unsigned long long ticks, cntfrq = get_cntfrq();
 	unsigned long long start = get_cntpct();
 
-	ticks = DIV_ROUND_DOWN_ULL((us * cntfrq), 1000000);
+	ticks = DIV_ROUND_DOWN_ULL((us * cntfrq), MSECOND);
 
 	while ((long)(start + ticks - get_cntpct()) > 0)
 		;
+}
+
+uint64_t get_time_ns(void)
+{
+	return get_cntpct() * SECOND / get_cntfrq();
+}
+
+int is_timeout(uint64_t start, uint64_t time_offset_ns)
+{
+	if ((int64_t)(start + time_offset_ns - get_time_ns()) < 0) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
