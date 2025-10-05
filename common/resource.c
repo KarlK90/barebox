4e70ab0f03bf4cfe5048f508f30f1791584720f9
Ahmad Fatoum <a.fatoum@barebox.org>
2025-10-05 12:55:11 +0200

    resource: rework __merge_regions for more general use
    __merge_regions so far always merged the second argument into the first
    and freed the second on success.
    
    To make this behavior more intuitive, return the resource that's not
    freed. While at it, also handle NULL arguments correctly to remove
    special casing in future callers.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

--- common/resource.c
+++ common/resource.c
@@ -111,11 +111,16 @@ int release_region(struct resource *res)
 /*
  * merge two adjacent sibling regions.
  */
-int __merge_regions(const char *name,
-		struct resource *resa, struct resource *resb)
+struct resource *__merge_regions(const char *name,
+				 struct resource *resa, struct resource *resb)
 {
+	if (!resa)
+		return resb;
+	if (!resb)
+		return resa;
+
 	if (!resource_adjacent(resa, resb))
-		return -EINVAL;
+		return NULL;
 
 	if (resa->start < resb->start)
 		resa->end = resb->end;
@@ -126,7 +131,7 @@ int __merge_regions(const char *name,
 	resa->name = xstrdup_const(name);
 	release_region(resb);
 
-	return 0;
+	return resa;
 }
 
 /* The root resource for the whole memory-mapped io space */
